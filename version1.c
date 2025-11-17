e480207ad3abf63ac2ab79b0e63d5c65e64f7806
Holden Roaten <hroaten@Holdens-MacBook-Pro-170.local>
2025-11-17 11:05:55 -0500

    git sanity check- added empty files for first versiions of project, raw_text_input folder for testing

--- version1.c
+++ version1.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
 
+/*
+Open MP version of wordcount-mapreduce
+*/
+
+int main(int argc, char**argv){
+
+}
--- version2.c
+++ version2.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<mpi.h>
+#include<omp.h>
 
+/*
+OpenMP-MPI hybrid wordcount-reduce
+*/
+
+int main(int argc, char** argv) {}
